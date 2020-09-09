452c4bf62efdbf4f2e56648960866ea49a42383d
Rodrigo Gonzalez Jorquera <rgonzalezj@gmail.com>
2020-09-09 20:22:37 -0300

    Create ventas.rb

---
+++ ventas.rb
@@ -0,0 +1,27 @@
+=begin
+Dado un hash crear un método que devuelva otro hash pero filtrando todos aquellos que tienen
+valores inferior a 70000
+Probar con el siguiente hash:
+ventas = {
+Octubre: 65000,
+Noviembre: 68000,
+Diciembre: 72000
+}   
+=end
+
+
+def filter(hash)
+    filtered_hash = {}
+    hash.each do |month,value|
+        if value < 70000
+            filtered_hash[month]= value
+        end
+    end
+    filtered_hash
+end
+
+    filter({
+        Octubrer: 65000,
+        November: 68000,
+        December: 72000
+    })
